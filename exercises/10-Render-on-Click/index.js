11c911054f8396ed418ef3fcd146de154035cbff
LaburgosCR <luis.larraburo92@gmail.com>
2021-01-26 23:06:38 +0000

    ejercicios

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
-
+	var element = document.createElement("P");
+	element.innerHTML = "Hello World";
+	element.style.background = "yellow";
+	document.body.appendChild(element);
 });
--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	var elemento = document.createElement("li");
+	var texto = document.createTextNode("Elemento de prueba");
+	elemento.appendChild(texto);
+	
 });
