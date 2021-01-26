8d703eb161e3824cb129c3920ea52cf7c80b2d45
LaburgosCR <luis.larraburo92@gmail.com>
2021-01-26 18:08:36 +0000

    Practica DOM

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-var element = document.createElement("P");
-element.innerHTML = "Hello World";
-element.style.background = "yellow";
-document.querySelector("#myDiv").appendChild(element);
